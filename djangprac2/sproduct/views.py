30241b73ffcb6dcd6f014acc5bd113b1bb03f23d
HyeokSeong Kwon <khs50851@naver.com>
2020-12-05 19:49:44 +0900

    상품 상세보기 + 위지윅

--- djangprac2/sproduct/views.py
+++ djangprac2/sproduct/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Sproduct
 from .forms import RegisterForm
@@ -16,3 +16,11 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Sproduct.objects.all()  # 어떤 모델이 아니라 쿼리셋을 지정해서 넣어야함
+    # 조건 지정시 필터로 조건 지정할수있음
+    print("쿼리셋 : ", queryset)
+    context_object_name = 'product'
