7eb7a54714f731a4b8e05d41483dbbbebd8c4a48
HyeokSeong Kwon <khs50851@naver.com>
2020-12-07 16:28:28 +0900

    주문에 커스텀필드 추가

--- djangprac2/sorder/admin.py
+++ djangprac2/sorder/admin.py
@@ -4,7 +4,12 @@ from .models import Sorder
 
 
 class SorderAdmin(admin.ModelAdmin):
-    list_display = ('suser', 'product')
+    list_filter = ('status',)
+    # 스타일된 스테이터스를 함수로 만들수있음
+    list_display = ('suser', 'product', 'styled_status')
+
+    def styled_status(self, obj):  # 이 함수를 만들고 등록 obj는 각 레코드를 의미
+        return obj.status
 
 
 admin.site.register(Sorder, SorderAdmin)
--- djangprac2/sorder/models.py
+++ djangprac2/sorder/models.py
@@ -9,6 +9,18 @@ class Sorder(models.Model):
     product = models.ForeignKey(
         'sproduct.Sproduct', on_delete=models.CASCADE, verbose_name='상품')
     quantity = models.IntegerField(verbose_name='수량')
+    status = models.CharField(
+        choices=(
+            ('대기중', '대기중'),
+            ('결제대기', '결제대기'),
+            ('결제완료', '결제완료'),
+            ('환불', '환불'),  # 첫번쨰는 데이터베이스에 저장되는값, 두번쨰는 사용자에게 보여질값
+        ),
+        default='대기중',
+        max_length=32,
+        verbose_name='상태'
+    )
+    memo = models.TextField(null=True, blank=True, verbose_name='메모')
     regdate = models.DateTimeField(auto_now_add=True, verbose_name='등록일')
 
     def __str__(self):  # admin에서 ~~.object라고 나오는걸 구체적으로 바꿈
